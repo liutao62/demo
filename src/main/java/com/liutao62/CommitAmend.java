1224b336c1da612dadb506ceaa2e035df2641a2b
liutao62 <627224125@qq.com>
2020-07-30 16:00:56 +0800

    commit amend

---
+++ src/main/java/com/liutao62/CommitAmend.java
@@ -0,0 +1,9 @@
+package com.liutao62;
+
+/**
+ * @author liutao
+ * @date Created in 2020/7/30 15:55
+ * @description
+ */
+public class CommitAmend {
+}
---
+++ src/main/java/com/liutao62/CommitAmend1.java
@@ -0,0 +1,9 @@
+package com.liutao62;
+
+/**
+ * @author liutao
+ * @date Created in 2020/7/30 15:59
+ * @description
+ */
+public class CommitAmend1 {
+}
