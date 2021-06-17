c700fd3abe224caa2a6fe3d033e1d5ac3ec7854c
liutao62 <627224125@qq.com>
2021-06-17 23:31:07 +0800

    回首掏

--- src/main/java/com/liutao62/leetcode/_206reverseList.java
+++ src/main/java/com/liutao62/leetcode/_206reverseList.java
@@ -1,10 +1,7 @@
 package com.liutao62.leetcode;
 
-import java.util.LinkedList;
-
 public class _206reverseList {
     public static ListNode reverseList(ListNode head) {
-        LinkedList<ListNode> queue = new LinkedList<>();
         ListNode tem = null, cursor;
         while (head != null) {
             cursor = head.next;
