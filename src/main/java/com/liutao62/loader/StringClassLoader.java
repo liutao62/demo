fdc5f60fd197f388faff46a418ec05c368462870
liutaoq <liutaoq@yonyou.com>
2025-01-02 14:29:46 +0800

    热编译

--- src/main/java/com/liutao62/loader/StringClassLoader.java
+++ src/main/java/com/liutao62/loader/StringClassLoader.java
@@ -2,6 +2,15 @@ package com.liutao62.loader;
 
 import com.google.common.collect.Maps;
 import com.itranswarp.compiler.JavaStringCompiler;
+import org.codehaus.commons.compiler.CompilerFactoryFactory;
+import org.codehaus.commons.compiler.ICompiler;
+import org.codehaus.commons.compiler.ICompilerFactory;
+import org.codehaus.commons.compiler.util.ResourceFinderClassLoader;
+import org.codehaus.commons.compiler.util.resource.MapResourceCreator;
+import org.codehaus.commons.compiler.util.resource.MapResourceFinder;
+import org.codehaus.commons.compiler.util.resource.Resource;
+import org.codehaus.commons.compiler.util.resource.StringResource;
+import org.springframework.util.StopWatch;
 
 import java.lang.reflect.Method;
 import java.util.HashMap;
@@ -29,6 +38,8 @@ public class StringClassLoader {
             "}";
 
     public static void main(String[] args) throws Exception {
+        StopWatch stopWatch = new StopWatch("compilier");
+        stopWatch.start("JavaStringCompiler");
         JavaStringCompiler compiler = new JavaStringCompiler();
         String scriptClassName = "StringScript";
         Map<String, byte[]> results = compiler.compile(scriptClassName + ".java", script);
@@ -39,5 +50,48 @@ public class StringClassLoader {
         HashMap<String, Object> hashMap = Maps.newHashMap();
         hashMap.put("1111", 22222);
         eval.invoke(null, hashMap);
+        stopWatch.stop();
+
+        stopWatch.start("ICompilerFactory");
+
+        ICompilerFactory defaultCompilerFactory = CompilerFactoryFactory.getDefaultCompilerFactory(ClassLoader.getSystemClassLoader());
+
+        ICompiler compilerNew = defaultCompilerFactory.newCompiler();
+
+// Store generated .class files in a Map:
+        Map<String, byte[]> classes = new HashMap<String, byte[]>();
+        compilerNew.setClassFileCreator(new MapResourceCreator(classes));
+
+// Now compile two units from strings:
+//        compilerNew.compile(new Resource[]{
+//                new StringResource(
+//                        "pkg1/A.java",
+//                        "package pkg1; public class A { public static int meth() { return pkg2.B.meth(); } }"
+//                ),
+//                new StringResource(
+//                        "pkg2/B.java",
+//                        "package pkg2; public class B { public static int meth() { return 77;            } }"
+//                ),
+//        });
+
+        compilerNew.compile(new Resource[]{
+                new StringResource(
+                        scriptClassName + ".java",
+                        script
+                )
+        });
+
+// Set up a class loader that uses the generated classes.
+        ClassLoader cl = new ResourceFinderClassLoader(
+                new MapResourceFinder(classes),    // resourceFinder
+                ClassLoader.getSystemClassLoader() // parent
+        );
+
+        hashMap.put("1111", 33333);
+        Class<?> aClass = cl.loadClass(packagePrefix + "." + scriptClassName);
+        Object meth = aClass.getDeclaredMethod("eval", Map.class).invoke(null, hashMap);
+        System.out.println(meth);
+        stopWatch.stop();
+        System.out.println(stopWatch.prettyPrint());
     }
 }
